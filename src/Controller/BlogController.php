3ac78834b23c811099a0dd3278e4dea51a4c02e9
sylpi <syl.pillet@hotmail.fr>
2020-10-21 22:54:03 +0200

    comment form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Comment;
 use App\Entity\Post;
-use App\Repository\CommentRepository;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Laminas\EventManager\EventManagerInterface;
 use Symfony\Component\VarDumper\Cloner\Data;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Date;
@@ -23,21 +27,31 @@ class BlogController extends AbstractController
      */
     public function index(PostRepository $repo): Response
     {
-       $posts = $repo->getPostsAndComs();
-
+        $posts = $repo->getPostsAndComs();
+      
         return $this->render('home/index.html.twig', [
-            'posts' => $posts
+            'posts' => $posts,
+            
         ]);
     }
     
     /**
      * @Route("/posts/{id}", name="detail")
      */
-    public function detail(Post $post)
+    public function detail(Post $post, Request $request, EntityManagerInterface $manager)
     {
 
+        $comment = new Comment();
+        $comment->setPost($post);
+        $comment->setPostedAt(new \DateTime);
+        $form = $this->createForm(CommentType::class, $comment)->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+        }
         return $this->render('home/detail.html.twig', [
-            'post' => $post
+            'post' => $post,
+            'form' => $form->createView()
         ]);
     }
 }
