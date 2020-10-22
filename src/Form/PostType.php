149e60b699ec2b0b371f91d8613522dab39f9d75
sylpi <syl.pillet@hotmail.fr>
2020-10-22 23:24:51 +0200

    regroup add & update form in _form.html.twig

---
+++ src/Form/PostType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author', TextType::class, [
+                'label' => 'name'
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Title:'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Text:'
+            ])
+            // ->add('createdAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
